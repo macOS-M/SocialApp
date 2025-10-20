8b13999c36144ef15fdceab6046f859d27d1e215
Marcos Morales <mmorales99c@gmail.com>
2025-10-20 08:44:48 -0600

    eslint fix

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["flash"]
+  static targets = ["flash"];
 
   connect() {
     this.flashTargets.forEach((el) => {
-      setTimeout(() => this.dismiss(el), 3000)
-    })
+      setTimeout(() => this.dismiss(el), 3000);
+    });
   }
 
   dismiss(el) {
-    el.style.transition = "opacity 0.5s, max-height 0.5s"
-    el.style.opacity = "0"
-    el.style.maxHeight = "0px"
-    setTimeout(() => el.remove(), 500)
+    el.style.transition = "opacity 0.5s, max-height 0.5s";
+    el.style.opacity = "0";
+    el.style.maxHeight = "0px";
+    setTimeout(() => el.remove(), 500);
   }
 }

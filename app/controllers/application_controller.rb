e2e622b69fc570a5b1eca7dea03861204aa9331a
Marcos Morales <mmorales99c@gmail.com>
2025-10-21 11:20:01 -0600

    removed duplicated logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     dashboard_path
   end
-
-  def after_update_path_for(resource)
-    profile_path(resource.username)
-  end
 end

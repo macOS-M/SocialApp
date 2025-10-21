ab92176752202fd35ad16afec3d7c322ca39d0d1
Marcos Morales <mmorales99c@gmail.com>
2025-10-21 11:15:25 -0600

    Added profile personalization, posts now show profile picture, minor style changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,23 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :date_of_birth ])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :date_of_birth ])
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [
+        :username,
+        :date_of_birth,
+        :bio,
+        :location,
+        :profile_image,
+        :cover_image
+      ]
+    )
   end
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def after_update_path_for(resource)
+    profile_path(resource.username)
   end
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,7 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_update_path_for(resource)
+    profile_path(resource.username)
+  end
+end
